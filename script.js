ad14e46284f26f9c9d9e1fa6590193fd51e9d591
Maxim Arzhanov <Arzhanov93@yandex.ru>
2021-07-24 13:24:24 +0300

    feat: Написан script

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-let popupEditProfile = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let closePopup = document.querySelector('.popup__icon-close');
-
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
+let editButton = document.querySelector('.profile__edit-button');
 
+let popupEditProfile = document.querySelector('.popup');
 let popupInputName = document.querySelector('.popup__input-name');
 let popupInputDescription = document.querySelector('.popup__input-description');
+let popupButton = document.querySelector('.popup__button');
+let formElement = document.querySelector('.popup__form');
+let closePopup = document.querySelector('.popup__icon-close');
 
-popupInputName.value = profileName.textContent;
-popupInputDescription.value = profileDescription.textContent;
-
+// Записывает имя и описание в поля формы всплывающего окна
 function fillFields() {
   popupInputName.value = profileName.textContent;
   popupInputDescription.value = profileDescription.textContent;
 }
 
+// Открытие/Закрытие popup
 function openClosePopup() {
   popupEditProfile.classList.toggle('popup_opened');
   fillFields();
 }
 
+// Открытие/Закрытие popup
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = popupInputName.value;
+  profileDescription.textContent = popupInputDescription.value;
+  openClosePopup();
+}
+
 editButton.addEventListener('click', openClosePopup);
 closePopup.addEventListener('click', openClosePopup);
-
+formElement.addEventListener('submit', formSubmitHandler);
