884ebbe8600dc4adb1772c98743500d203d54bf2
Maxim Arzhanov <Arzhanov93@yandex.ru>
2021-07-25 14:54:33 +0300

    fix: Функция openClosePopup разделена на две функции - openPopup и closePopup

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,38 @@
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-let editButton = document.querySelector('.profile__edit-button');
+let buttonEditProfile = document.querySelector('.profile__edit-button');
 
 let popupEditProfile = document.querySelector('.popup');
 let popupInputName = document.querySelector('.popup__input_type_name');
 let popupInputDescription = document.querySelector('.popup__input_type_description');
 let formElement = document.querySelector('.popup__form');
-let closePopup = document.querySelector('.popup__icon-close');
+let buttonClosePopup = document.querySelector('.popup__icon-close');
 
-// Записывает имя и описание в поля формы всплывающего окна
+/** Записывает имя и описание в поля формы всплывающего окна. */
 function fillFields() {
   popupInputName.value = profileName.textContent;
   popupInputDescription.value = profileDescription.textContent;
 }
 
-// Открытие/Закрытие popup
-function openClosePopup() {
-  popupEditProfile.classList.toggle('popup_opened');
+/** Открывает popup. */
+function openPopup() {
+  popupEditProfile.classList.add('popup_opened');
   fillFields();
 }
 
-// Сохранить имя и описание в профиле
+/** Закрывает popup. */
+function closePopup() {
+  popupEditProfile.classList.remove('popup_opened');
+}
+
+/** Сохраняет имя и описание в профиле. */
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = popupInputName.value;
   profileDescription.textContent = popupInputDescription.value;
-  openClosePopup();
+  closePopup();
 }
 
-editButton.addEventListener('click', openClosePopup);
-closePopup.addEventListener('click', openClosePopup);
+buttonEditProfile.addEventListener('click', openPopup);
+buttonClosePopup.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
