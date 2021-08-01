6243bd5fd27327acfce4b78d5e89c04c24e1b13e
Maxim Arzhanov <Arzhanov93@yandex.ru>
2021-08-01 16:05:50 +0300

    feat: Создан popup для  добавления новых элементов на страницу

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,61 @@
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let buttonEditProfile = document.querySelector('.profile__edit-button');
+let buttonAddNewPlace = document.querySelector('.profile__add-button');
 
-let popupEditProfile = document.querySelector('.popup');
-let popupInputName = document.querySelector('.popup__input_type_name');
-let popupInputDescription = document.querySelector('.popup__input_type_description');
-let formElement = document.querySelector('.popup__form');
-let buttonClosePopup = document.querySelector('.popup__icon-close');
+let popupEditProfile = document.querySelector('.popup_type_edit-profile');
+let buttonCloseEditProfile = popupEditProfile.querySelector('.popup__icon-close');
+let popupInputNameProfile = popupEditProfile.querySelector('.popup__input_type_name');
+let popupInputDescription = popupEditProfile.querySelector('.popup__input_type_description');
+let formElementEditProfile = popupEditProfile.querySelector('.popup__form');
+
+let popupAddNewPlace = document.querySelector('.popup_type_add-new-place');
+let buttonCloseAddNewPlace = popupAddNewPlace.querySelector('.popup__icon-close');
+let popupInputNamePicture = popupAddNewPlace.querySelector('.popup__input_type_name');
+let popupInputLink = popupAddNewPlace.querySelector('.popup__input_type_description');
+let formElementAddNewPlace = popupAddNewPlace.querySelector('.popup__form');
 
 /** Записывает имя и описание в поля формы всплывающего окна. */
 function fillFields() {
-  popupInputName.value = profileName.textContent;
+  popupInputNameProfile.value = profileName.textContent;
   popupInputDescription.value = profileDescription.textContent;
 }
 
 /** Открывает popup. */
-function openPopup() {
-  popupEditProfile.classList.add('popup_opened');
-  fillFields();
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
 /** Закрывает popup. */
-function closePopup() {
-  popupEditProfile.classList.remove('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 /** Сохраняет имя и описание в профиле. */
-function formSubmitHandler (evt) {
+function formSubmitHandler (evt, popup) {
   evt.preventDefault();
-  profileName.textContent = popupInputName.value;
+  profileName.textContent = popupInputNameProfile.value;
   profileDescription.textContent = popupInputDescription.value;
-  closePopup();
+  closePopup(popup);
 }
 
-buttonEditProfile.addEventListener('click', openPopup);
-buttonClosePopup.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+buttonEditProfile.addEventListener('click', () => {
+  openPopup(popupEditProfile);
+  fillFields();
+});
+
+buttonAddNewPlace.addEventListener('click', () => {
+  openPopup(popupAddNewPlace);
+});
+
+buttonCloseEditProfile.addEventListener('click', () => {
+  closePopup(popupEditProfile);
+});
+
+buttonCloseAddNewPlace.addEventListener('click', () => {
+  closePopup(popupAddNewPlace);
+});
+
+formElementEditProfile.addEventListener('submit', evt => {
+  formSubmitHandler(evt, popupEditProfile);
+});
